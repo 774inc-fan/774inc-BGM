5d9795729d763a70908417043dfa88b282c304d4
ozelotjp <contact@ozelot.app>
2020-06-18 22:14:34 +0900

    feat: ホームページを追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,39 +2,91 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-card>
-          <v-card-title>
-            Hello World
-          </v-card-title>
-          <v-card-text>
-            <p>Now Time: {{ now() }}</p>
-          </v-card-text>
-        </v-card>
+        <h1>
+          ホーム
+        </h1>
+        <p>
+          推しの配信で流れてるBGMをいつでも聞けたら、作業が捗るのでは？という安直な考えで生まれたWebアプリです。
+        </p>
+        <h2>
+          使い方
+        </h2>
+        <p>
+          サイドバーからメンバーを選択してください。スマートフォンをご利用の場合は、左上の横棒３本を押すと出てきます。
+        </p>
+        <h2>
+          提案／指摘／その他お問い合わせ
+        </h2>
+        <p>
+          オセロット（
+          <a href="https://twitter.com/ozelotjp_v">@ozelotjp_v</a>
+          ）まで連絡ください。
+        </p>
+        <h2>
+          メモ
+        </h2>
+        <p>
+          何か追加しといたほうがいいやつあったら教えて下さい
+        </p>
+        <h3>
+          あにまーれで使われたサイト
+        </h3>
+        <ul>
+          <li>
+            beco (騒音のない世界) https://noiselessworld.net
+          </li>
+          <li>
+            音楽素材MusMus http://musmus.main.jp
+          </li>
+          <li>
+            甘茶の音楽工房 http://amachamusic.chagasi.com/index.html
+          </li>
+          <li>
+            DOVA-SYNDROME https://dova-s.jp
+          </li>
+          <li>
+            PeriTune https://peritune.com/
+          </li>
+        </ul>
+        <h3>
+          他のVTuberが使ってたりするサイト
+        </h3>
+        <ul>
+          <li>
+            H/MIX GALLERY http://www.hmix.net/
+          </li>
+          <li>
+            d-elf.com https://www.d-elf.com/
+          </li>
+          <li>
+            Music-Note.jp http://www.music-note.jp/index.html
+          </li>
+          <li>
+            OtoLogic https://otologic.jp/
+          </li>
+          <li>
+            PANICPUMPKIN http://pansound.com/panicpumpkin/index.html
+          </li>
+          <li>
+            こんとどぅふぇ http://conte-de-fees.com/
+          </li>
+          <li>
+            ポケットサウンド https://pocket-se.info/
+          </li>
+          <li>
+            音の園 http://oto-no-sono.com/about.html
+          </li>
+          <li>
+            音楽の卵 http://ontama-m.com/index.html
+          </li>
+          <li>
+            騒音のない世界 https://noiselessworld.net/
+          </li>
+          <li>
+            魔王魂 https://maoudamashii.jokersounds.com/
+          </li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
-<script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-  middleware: 'authenticated',
-  setup() {
-    const now = () => {
-      const date = new Date()
-      const y = date.getFullYear()
-      const m = ('0' + (date.getMonth() + 1)).slice(-2)
-      const d = ('0' + date.getDate()).slice(-2)
-      const h = ('0' + date.getHours()).slice(-2)
-      const i = ('0' + date.getMinutes()).slice(-2)
-      const s = ('0' + date.getSeconds()).slice(-2)
-      return `${y}-${m}-${d} ${h}:${i}:${s}`
-    }
-
-    return {
-      now
-    }
-  }
-})
-</script>
