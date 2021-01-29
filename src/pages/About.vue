b2c6a1464ed9f88c3df0e6cc50872e138cfb1ee0
ozelotjp <contact@ozelot.app>
2021-01-29 16:53:19 +0900

    feat(pages): Aboutページを実装

---
+++ src/pages/About.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page padding>
+    <h1 class="app-h1">
+      このサイトについて
+    </h1>
+    <p>
+      774incのVtuberが使っているBGMをまとめました。作業のお供や寝るときなど、様々な場面で活躍します。
+    </p>
+
+    <h2 class="app-h2">
+      誰が作っていますか？
+    </h2>
+    <p>
+      Webサイトはオセロットが制作しています。<br>
+      BGMに関する情報はオセロットを含む有志の人たちで調べています。
+    </p>
+
+    <h2 class="app-h2">
+      私の知っているBGMがWebサイトにないのですが？
+    </h2>
+    <p>
+      もしよろしければ、下記の情報提供フォームからご連絡ください！
+    </p>
+    <q-btn type="a" href="https://forms.gle/3F384gQx8YWztL5S6" target="_blank" color="primary" icon="ion-mail" label="情報提供フォームはこちら" />
+
+    <h2 class="app-h2">
+      お問い合わせ
+    </h2>
+    <p>
+      Twitterで直接リプライを送るか、総合お問い合わせフォームをご利用ください。
+    </p>
+    <div class="q-gutter-sm">
+      <q-btn type="a" href="https://twitter.com/ozelotjp_v" target="_blank" color="primary" icon="ion-logo-twitter" label="Twitterアカウントはこちら" no-caps />
+      <q-btn type="a" href="https://docs.google.com/forms/d/e/1FAIpQLSdDSD2L1HlDBx9obWEX5H6nXWmgVggcjAdqyokebFuvazEj7g/viewform?usp=pp_url&entry.309446658=bgm.774inc.app" target="_blank" color="primary" icon="ion-mail" label="お問い合わせフォームはこちら" />
+    </div>
+  </q-page>
+</template>
