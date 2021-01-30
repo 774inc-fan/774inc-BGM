bcac6e2e3d30bcd4a22c204d8472439f9aea1354
ozelotjp <contact@ozelot.app>
2021-01-30 22:41:38 +0900

    feat(pages): デバッグページの実装

---
+++ src/pages/Debug.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page padding>
+
+    <h1 class="app-h1">
+      Debug
+    </h1>
+
+    <h2 class="app-h2">
+      Check for duplicate musics
+    </h2>
+    <div v-if="conflictYoutube.length === 0" class="app-alert app-alert--tip">
+      <p>No issue detected🎉</p>
+    </div>
+    <div v-else class="app-alert app-alert--danger">
+      <ul>
+        <li v-for="(music, _) in conflictYoutube" :key="_">
+          {{ music.youtube }}
+        </li>
+      </ul>
+    </div>
+
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { musicData } from 'app/data/musics'
+
+export default defineComponent({
+  setup () {
+    const conflictYoutube = musicData
+      .filter((music) => {
+        return musicData.filter((x) => x.youtube === music.youtube).length > 1
+      })
+
+    return { conflictYoutube }
+  }
+})
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,8 @@ const routes: RouteConfig[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '/', component: () => import('pages/Index.vue') },
-      { path: '/about', component: () => import('pages/About.vue') }
+      { path: '/about', component: () => import('pages/About.vue') },
+      { path: '/debug', component: () => import('pages/Debug.vue') }
     ]
   },
 
